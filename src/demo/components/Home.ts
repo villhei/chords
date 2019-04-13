ded8e64c08003b92c588a8b67abb8cf9f7d164bb
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2019-04-13 12:23:15 +0300

    Add deferred autoplay

--- src/demo/components/Home.ts
+++ src/demo/components/Home.ts
@@ -18,8 +18,8 @@ async function getMediaFileAsStream(
   const audio = new Audio(url)
   audio.muted = isMuted
   audio.loop = true
-  audio.playbackRate = 1.0
   await audio.play()
+  audio.playbackRate = 1.0
   return audio.captureStream()
 }
 
@@ -35,33 +35,47 @@ const canvasSize = {
 
 const Home = (iNode: m.Vnode): m.Component => {
   const microphoneStream: Promise<MediaStream> = getMicrophoneStream()
+  let allowPlayback = false
 
-  const mediaView = [sineSweepUrl, sineSweepFullRangeUrl]
-    .map(fileName => [fileName, getMediaFileAsStream(fileName)])
-    .map(([fileName, stream]) => [
-      m('h3', 'Datasource: ' + formatName(fileName)),
-      m('checkbox', {
-        checked: false
-      }),
-      m('h4', 'Time domain'),
-      m(Visualizer, {
-        ...canvasSize,
-        stream
-      }),
-      m('h4', 'Frequency domain'),
-      m(FrequencyVisualizer, {
-        ...canvasSize,
-        stream
-      })
-    ])
-    .reduce((acc, item) => acc.concat(...item), [])
   return {
+    oncreate(vnode: m.VnodeDOM) {
+      const startPlayback = () => {
+        allowPlayback = true
+        document.removeEventListener('click', startPlayback)
+        m.redraw()
+      }
+      document.addEventListener('click', startPlayback)
+    },
     view(vnode) {
+      const mediaView = [sineSweepUrl, sineSweepFullRangeUrl]
+        .filter(() => allowPlayback)
+        .map(fileName => [fileName, getMediaFileAsStream(fileName)])
+        .map(([fileName, stream]) => [
+          m('h3', 'Datasource: ' + formatName(fileName)),
+          m('checkbox', {
+            checked: false
+          }),
+          m('h4', 'Time domain'),
+          m(Visualizer, {
+            ...canvasSize,
+            stream
+          }),
+          m('h4', 'Frequency domain'),
+          m(FrequencyVisualizer, {
+            ...canvasSize,
+            stream
+          })
+        ])
+        .reduce((acc, item) => acc.concat(...item), [])
       return m(
         '.page',
         [
           m(Nav),
           m('h1', "Let's interface with audio"),
+          m(
+            'p',
+            'Click somewhere on the page so that muted autoplay can be started'
+          ),
           m('h3', 'Microphone input'),
           m(Visualizer, {
             ...canvasSize,
